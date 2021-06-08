56392518e864713e365bef79c07d5e9eda4b403c
Jonathan Lopes <63373963+jonathan-lopes@users.noreply.github.com>
2021-06-08 18:30:08 -0300

    Desafio  hackerrank tem jogo

---
+++ temJogo.js
@@ -0,0 +1,10 @@
+function solucao(ano) {
+    if (ano % 4 === 2)
+        console.log("COPA");
+    else if (ano % 4 === 0)
+        console.log("JOGOS");
+    else
+        console.log("MEH");
+}
+
+solucao(2025);
